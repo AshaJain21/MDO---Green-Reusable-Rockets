013dd5790619a835fad6f7d66c4b78e46c24d8a0
Justin <justinpws@gmail.com>
2023-03-11 15:37:26 -0500

    Reworked mission module with time-based simulation and adaptive production times

--- Mission_Module/MissionModule.m
+++ Mission_Module/MissionModule.m
@@ -4,33 +4,99 @@ classdef MissionModule
         num_sats %[# satellites]
         fleet_size %[# launch vehicles]
         del_window %[months]
-        sat_prod_time %[months per satellite]
-        booster_refurb_time %[months per booster]
-        booster_prod_time %[months per booster]
+        sat_prod_times %[months per satellite]
+        size_ubs %size bounds for the rocket size bins
+        launcher_refurb_time %[months per launcher]
+        launcher_prod_times %[months per launcher]
+        LR %learning rate
+
     end
     methods
-        function obj = MissionModule(sat_mass, num_sats, fleet_size, del_window, sat_prod_rate, booster_refurb_time, booster_prod_time)
+        function obj = MissionModule(sat_mass, num_sats, fleet_size, del_window, init_sat_prod_time, booster_refurb_time, LR)
+            
             obj.sat_mass = sat_mass;
             obj.num_sats = num_sats;
             obj.fleet_size = fleet_size;
             obj.del_window = del_window;
-            obj.sat_prod_time = 1/sat_prod_rate;
-            obj.booster_refurb_time = booster_refurb_time;
-            obj.booster_prod_time = booster_prod_time;
+            obj.launcher_refurb_time = booster_refurb_time;
+            obj.LR = LR;
+
+            sat_vec =  1:obj.num_sats;
+            obj.sat_prod_times = init_sat_prod_time*sat_vec.^(log(obj.LR)/log(2));
+
+            obj.size_ubs = [3,6,9]; %1st bin driven by electron, 2nd bin driven by falcon 9, large bin driven by SLS
+            obj.launcher_prod_times = [0.5, 0.5, 36];
         end
-        function [per_launch_mass, launch_cadence] = Run(obj, stg1_reuse, stg2_reuse, num_launches)
+        function [per_launch_mass, launch_cadences, additional_rockets_available] = Run(obj, stg1_reuse, stg2_reuse, num_launches, launcher_radius)
+            
+            %CALCULATE MASS PER LAUNCH (DIVIDE EVENLY)
             num_sat_per_launch = ceil(obj.num_sats / num_launches);
             per_launch_mass = obj.sat_mass * num_sat_per_launch;
-            
-            sat_prod_time_per_launch = num_sat_per_launch * obj.sat_prod_time
-            
-            if num_launches <= obj.fleet_size
-                launch_cadence = sat_prod_time_per_launch; %Assume that you can launch as quickly as satellites are produced
-            else %NOT WORKING BELOW THIS LINE
-                if stg1_reuse==true
-                    launch_cadence = ((num_launches-obj.fleet_size)*obj.booster_prod_time*obj.booster_refurb_time) / ( (num_launches-1)*(obj.booster_refurb_time + obj.booster_prod_time)); 
-                else
-                    launch_cadence = ((num_launches-obj.fleet_size)*obj.booster_prod_time) / (num_launches-1);
+
+            %DETERMINE LAUNCHER PRODUCTION TIME BASED ON REQUIRED LAUNCHER
+            %RADIUS
+            for i=1:length(obj.size_ubs)
+                size_cat=obj.size_ubs(i);
+                if launcher_radius <= size_cat
+                    launcher_prod_time = obj.launcher_prod_times(i);
+                    break
+                end
+            end
+
+%             fprintf('============Starting Numbers============\n')
+            %SET UP TRACKING VARIABLES FOR SIMULATION
+            num_sats_produced = 0;
+            num_sats_awaiting_launch = 0;
+            num_sats_launched = 0;
+            num_launchers_available = obj.fleet_size;
+            next_sat_prod_time=obj.sat_prod_times(1);
+            next_launcher_prod_time = launcher_prod_time;
+            next_launcher_refurb_time = obj.launcher_refurb_time;
+            curr_time_step = 0;
+            last_launch_time = 0;
+            launch_cadences = [];
+            additional_rockets_available = 0;
+            run_count = 0;
+
+            while num_sats_launched < obj.num_sats
+                
+                run_count = run_count + 1;
+                curr_time_step = min([next_sat_prod_time, next_launcher_prod_time, next_launcher_refurb_time]);
+
+                % PRODUCE WHATEVER SHOULD BE PRODUCED AT THIS TIME
+                if curr_time_step == next_sat_prod_time
+%                     fprintf('Satellite being produced!')
+                    num_sats_produced = num_sats_produced + 1;
+                    num_sats_awaiting_launch = num_sats_awaiting_launch + 1;
+                    if num_sats_produced < obj.num_sats
+                        next_sat_prod_time = curr_time_step + obj.sat_prod_times(num_sats_produced+1);
+                    end
+                end
+
+
+                if curr_time_step == next_launcher_prod_time
+%                     fprintf('Launcher being produced!')
+                    num_launchers_available = num_launchers_available + 1;
+                    next_launcher_prod_time = curr_time_step + launcher_prod_time;
+                    additional_rockets_available = additional_rockets_available + 1;
+                end
+
+                if curr_time_step == next_launcher_refurb_time
+%                     fprintf('Launcher being refurbished!')
+                    num_launchers_available = num_launchers_available + 1;
+                    next_launcher_refurb_time = curr_time_step + obj.launcher_refurb_time;
+                    additional_rockets_available = additional_rockets_available + 1;
+                end
+
+                % CHECK TO SEE IF WE CAN LAUNCH ANYTHING YET (ASSUMES
+                % LAUNCH ON DEMAND)
+                if (num_sats_awaiting_launch >= num_sat_per_launch) && (num_launchers_available >= 1)
+%                     fprintf('============Launch!============\n')
+                    num_sats_launched = num_sats_launched + num_sat_per_launch;
+                    num_sats_awaiting_launch = num_sats_awaiting_launch - num_sat_per_launch;
+                    launch_cadences(end+1) = curr_time_step - last_launch_time;
+                    last_launch_time = curr_time_step;
+
                 end
             end
         end
--- Mission_Module/MissionTester.m
+++ Mission_Module/MissionTester.m
@@ -5,14 +5,28 @@ sat_mass = 1250; %[kg], v2.0 starlink satellites (from https://en.wikipedia.org/
 num_sats = 5280; %[# of satellites], based on 110 satellites per plane over 48 planes at 340km altitude (from https://www.nasaspaceflight.com/2022/12/spacex-starlink-5-1-launch/)
 del_window = 48; %[months] NEED REFERENCE
 fleet_size = 15; %[# of boosters, counted based on https://en.wikipedia.org/wiki/List_of_Falcon_9_first-stage_boosters counting just boosters awaiting assignment or ready for launch (boosters being refurbished not included)
-sat_prod_time = 120; %satellites per month (from https://www.cnbc.com/2020/08/10/spacex-starlink-satellte-production-now-120-per-month.html)
-booster_refurb_time = 60; %[days] (from rough numbers at https://www.quora.com/How-does-SpaceX-refurbish-their-Falcon-9-rockets-after-they-land-How-long-does-it-take)
+sat_prod_time = 0.025;%120; %satellites per month (from https://www.cnbc.com/2020/08/10/spacex-starlink-satellte-production-now-120-per-month.html)
+booster_refurb_time = 60/30; %[days] (from rough numbers at https://www.quora.com/How-does-SpaceX-refurbish-their-Falcon-9-rockets-after-they-land-How-long-does-it-take)
 booster_prod_time = 18; %[days] (from rough numbers at https://space.stackexchange.com/questions/10003/how-long-does-it-take-to-build-a-falcon-9-rocket)
+LR = 0.9;
 
-mm = MissionModule(sat_mass, num_sats, fleet_size, del_window, sat_prod_time, (booster_refurb_time/30), (booster_prod_time/30));
+mm = MissionModule(sat_mass, num_sats, fleet_size, del_window, sat_prod_time, booster_refurb_time, LR);
 
+fprintf('========================\n')
 %TEST MISSION 1
+fprintf('>> Test case where num_launches < fleet_size')
 stg1_reuse=true;
 stg2_reuse=true;
-num_launches=10;
-[per_launch_mass, launch_cadence] = mm.Run(stg1_reuse, stg2_reuse, num_launches)
+num_launches=2;
+[per_launch_mass, launch_cadences] = mm.Run(stg1_reuse, stg2_reuse, num_launches, 5)
+total_time = sum(launch_cadences)
+fprintf('\n========================\n')
+
+%TEST MISSION 2
+fprintf('>> Test case where num_launches > fleet_size')
+stg1_reuse=true;
+stg2_reuse=true;
+num_launches=30;
+[per_launch_mass, launch_cadences] = mm.Run(stg1_reuse, stg2_reuse, num_launches, 5)
+total_time = sum(launch_cadences)
+fprintf('========================\n')
