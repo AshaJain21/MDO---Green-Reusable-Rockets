3a7b322c17124660e93dc9c99f68e44985c3de96
Asha Jain <ashajain21@utexas.edu>
2023-03-13 14:56:55 -0400

    Added GWP 100 calculation to env module

--- Environment_Impact_Module/compute_rf.m
+++ Environment_Impact_Module/compute_rf.m
@@ -1,5 +1,4 @@
 function rf_per_species = compute_rf(combined_emissions)
-%Need to adjust with particle lifetime
 rf_per_species_perMg_map = containers.Map({'COx','H2O','Al2O3','BC'},{1.7e-8,3.2e-5,6e-3,3.4e-2}); %taken from https://strathprints.strath.ac.uk/82291/1/Calabuig_etal_EUCASS_2022_Eco_design_of_future_reusable_launchers.pdf
 species_keys = combined_emissions.keys();
 rf_per_species = containers.Map;
--- Environment_Impact_Module/run_testing.m
+++ Environment_Impact_Module/run_testing.m
@@ -1,5 +1,6 @@
 %Testing script for environment module 
+clc;clear;
 propellant_mass_burned_per_altitude = containers.Map({'Al2O3', 'NOx', 'COx'}, {[10, 20, 30; 10, 10, 10], [10, 20, 30; 10, 10, 10], [10, 20, 30; 10, 10, 10]});
 second_stage_reentry_mass = 5000;
 parameters.reusable_stage = [1,1];
-env_impact = run_env_impact_module(propellant_mass_burned_per_altitude, second_stage_reentry_mass, parameters);
+env_impact = run_env_impact_module(propellant_mass_burned_per_altitude, second_stage_reentry_mass, parameters)
