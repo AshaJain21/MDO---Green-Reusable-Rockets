fa904206b91660feba7452efd812705d29b00ad0
Asha Jain <ashajain21@utexas.edu>
2023-03-13 17:46:03 -0400

    Add refurb cost code

---
+++ Cost_Module/compute_refurb_cost.m
@@ -0,0 +1,23 @@
+function refurb_cost = compute_refurb_cost(stage_manuf_cost, launch_schedule, learning_curve)
+    %One option appears to take 30% of the stage cost as the
+    %refurbinshemnt cost - https://ojs.cvut.cz/ojs/index.php/mad/article/view/4855
+    %News article confirms that Space X stage 1 refurb cost is less
+    %than half of stage 1 manuf cost - https://spacenews.com/spacex-gaining-substantial-cost-savings-from-reused-falcon-9/
+    %Assumed that the first refurb cost would be 50% of stage cost
+    starting_percentage = 0.5;
+    num_launches = size(launch_schedule);
+    refurb_cost = zeros([1, num_launches(2)]);
+    refurbish_launch_count = 0;
+    for i = 1:num_launches
+        if launch_schedule(i, 2) == 1
+            stage_cost = stage_manuf_cost(i); 
+            refurbish_launch_count = refurbish_launch_count + 1;
+            learned_percentage = starting_percentage * refurbish_launch_count^learning_curve;
+            refurb_cost_for_stage = stage_cost * learned_percentage;
+            refurb_cost(i) = refurb_cost_for_stage;
+        end 
+
+    end
+
+
+end
--- Cost_Module/run_cost_module.m
+++ Cost_Module/run_cost_module.m
@@ -1,5 +1,8 @@
-function total_cost = run_cost_module(design_variables, parameters, rocket)
-    
+function total_cost = run_cost_module(design_variables, parameters, rocket, launch_schedule)
+    %Setting up array to track total cost for each launch
+    size_launch_schedule = size(launch_schedule);
+    total_cost_for_each_launch = zeros([1, size_launch_schedule(2)]);
+
     %Launcher material cost
     stg1_mat_volume = ((2*pi*rocket.stg1_radius*rocket.stg1_height) + (pi*srocket.stg1_radius^2)) * rocket.wall_thickness; %material volume of stage 1
     stg2_mat_volume = ((2*pi*rocket.stg2_radius*rocket.stg2_height) + (pi*srocket.stg2_radius^2)) * rocket.wall_thickness; %material volume of stage 2
@@ -7,6 +10,9 @@ function total_cost = run_cost_module(design_variables, parameters, rocket)
     launcher_mat_volume = stg1_mat_volume + stg2_mat_volume;
 
     launcher_mat_cost = launcher_mat_volume * parameters.launcher_mat_density * parameters.mat_unit_cost;
+    launcher_mat_costs_per_launcher = ones([1, size_launch_schedule(2)]) * launcher_mat_cost;
+    total_cost_for_each_launch = total_cost_for_each_launch + launcher_mat_costs_per_launcher;
+
     
 
     %Launcher manufacturing cost
@@ -14,10 +20,21 @@ function total_cost = run_cost_module(design_variables, parameters, rocket)
 
     %Launcher refurbishment cost
 
+    if parameters.reusable_stage(0) == 1
+        stage_1_refurb_cost= compute_refurb_cost(stages_1_manuf_cost, launch_schedule, parameters.refurb_cost_learning_curve);
+        total_cost_for_each_launch = total_cost_for_each_launch + stage_1_refurb_cost;
+    elseif parameters.reusable_stage(1) == 1
+        stage_2_refurb_cost= compute_refurb_cost(stages_2_manuf_cost, launch_schedule, parameters.refurb_cost_learning_curve);
+        total_cost_for_each_launch = total_cost_for_each_launch + stage_2_refurb_cost;
+    end
+
 
     %Propellant costs
     total_prop_cost = calculate_prop_cost(rocket.stg1_prop, rocket.stg1_prop_mass, parameters) + calculate_prop_cost(rocket.stg2_prop, rocket.stg2_prop_mass, parameters);
+    total_cost_for_each_launch =total_cost_for_each_launch + ones([1, size_of_launch_schedule(2)]) * total_prop_cost;
 
+    %Spreading costs over time (?) with dev costs amortized, this may boil
+    %down to maximum cost per launch 
 
-    total_cost = launcher_mat_cost + launcher_manuf_cost + launcher_refurb_cost + total_prop_cost;
+    total_cost = total_cost_for_each_launch;
 end
