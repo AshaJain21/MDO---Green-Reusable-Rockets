1fa930d850239bf79021100c825f9d55ed27a299
MaranCherry <126615966+MaranCherry@users.noreply.github.com>
2023-03-15 14:33:01 -0400

    Add files via upload
    Changed names to comply with updates structure naming

--- Structures_Module/Structures.m
+++ Structures_Module/Structures.m
@@ -2,7 +2,6 @@
 
 %Written By: Maranda and Kinjal
 
-
 %Inputs: Engine Thrust (Engine), Re-entry Shield Material, Structure
 %Material, St1&2 radius, structural mass fraction, propellant volume
 
@@ -14,19 +13,21 @@
 %body using the aluminum, and the height of the rocket for a given
 %propellant volume, get shield thickness required from the heat flux
 
-function [rocket, mass_heatshield] = structures(st1t, st2t, rocket, SMF, reentry_mat, struc_mat)
+function [rocket] = Structures(design_variables, parameters, rocket)
 %intialize constants and other vars
 FOS = 1.6; %factor of safety for load on rocket
 st1rad = rocket.stage1.radius;
 st2rad = rocket.stage2.radius;
-re_mat_density = reentry_mat.density; %[kg/m3] %add this on, ignore weight contribution by SMH
-re_mat_maxflux = reentry_mat.maxflux; %W/m2 max heat flux the ablative material can undergo w/out failure
-strucmat_density = struc_mat.density; %[kg/m3]
-sigma_max = struc_mat.fatstrength; %max fatigue stress of material MPA
+SMF = parameters.struc_to_propellant_mass_ratio;
+re_mat_density = design_variables.reentry_shield_material.density; %[kg/m3] %add this on, ignore weight contribution by SMH
+re_mat_maxflux = design_variables.reentry_shield_material.maxflux; %W/m2 max heat flux the ablative material can undergo w/out failure
+strucmat_density = paramters.structural_material.density; %[kg/m3]
+sigma_max = parameters.structural_material.fatigue_stress; %max fatigue stress of material MPA
 launch_qrock = rocket.stage1.launchheatflux;
 launch_q2 = rocket.stage2.launchheatflux;
 re_q1 = rocket.stage1.recoveryheatflux;
 re_q2 = rocket.stage2.recoveryheatflux;
+
 %heat flux - get max heat flux from calcs
 heatflux = [max(launch_qrock), max(launch_q2), max(re_q1), max(re_q2)]; %heat flux
 
@@ -46,11 +47,11 @@ wallthickness = max(wall);
 %for any other module?
 
 %rocket structural mass is a fraction of the propellant mass (cost calcs)
-rocket.stage1.strucmass = rocket.stage1.propmass*SMF;  
-rocket.stage2.strucmass = rocket.stage2.propmass*SMF; 
+rocket.stage1.structmass = rocket.stage1.propmass*SMF;  
+rocket.stage2.structmass = rocket.stage2.propmass*SMF; 
 
-st1volume = rocket.stage1.strucmass/strucmat_density; %volume of stage 1
-st2volume = rocket.stage2.strucmass/strucmat_density; %volume of stage 2
+st1volume = rocket.stage1.structmass/strucmat_density; %volume of stage 1
+st2volume = rocket.stage2.structmass/strucmat_density; %volume of stage 2
 
 %get height of each stage
 st1h = st1volume/(pi*(st1rad^2-R_inner1^2));%[m]
@@ -63,16 +64,14 @@ rocket.stage2.height = st2h; %[m]
 %this estimate may be too thick for now
 maxq = max(heatflux); %W/m^2 max heat flux at any point in flight
 
-if maxq > re_mat_maxflux %if max heat flux is higher than the shield capacity
-    
-    
+%if maxq > re_mat_maxflux %if max heat flux is higher than the shield capacity 
 
 reshieldthick = 0.0508; %[m] shield thickness 2 inches SHOULD THIS BE CALCULATED LATER?
 
 %get mass of shield material required for cost
 SAst2 = pi*st2rad^2*st2h; %only need it for the upper stage for re-entry
 vol_heatshield = SAst2*reshieldthick;
-mass_heatshield = vol_heatshield*re_mat_density; %[kg]
+rocket.stage2.heat_shield_mass = vol_heatshield*re_mat_density; %[kg]
 
 
 
