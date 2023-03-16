cc7cd8d6b375e750db2ad508fe260a30807cb93c
Justin <justinpws@gmail.com>
2023-03-16 17:17:05 -0400

    Added a couple of parameters for mission module to properly support reuse

--- setup_parameters.m
+++ setup_parameters.m
@@ -21,12 +21,13 @@ function parameters = setup_parameters()
     parameters.manuf_learning_rate= 0.9;
     parameters.sat_prod_learning_rate = 0.9;
 
-    parameters.launcher_refurb_time = 252; %shuttle avg, but space x ar 27 to 21 days. source: https://ark-invest.com/newsletters/issue-335/
+    parameters.launcher_refurb_times = [60, 30]; %shuttle avg, but space x ar 27 to 21 days. source: https://ark-invest.com/newsletters/issue-335/
     parameters.launcher_prod_time_bins = [3,6,9; 0.5,0.5, 36];
     %one web is manufc 2 sats per day: https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiaycuK89v9AhX7j4kEHbKxBusQFnoECBAQAw&url=https%3A%2F%2Fwww.floridatoday.com%2Fstory%2Ftech%2Fscience%2Fspace%2F2022%2F01%2F21%2Foneweb-satellites-look-inside-sci-fi-factory-kennedy-space-center-florida%2F6172817001%2F&usg=AOvVaw3NnmyaKJKqShZW8TTM5QS4
     %new record speed at 120 starlinks per month. source: https://www.cnbc.com/2020/08/10/spacex-starlink-satellte-production-now-120-per-month.html
     parameters.init_sat_prod_time = 30; %days, lets try this value and see if the learning rate drops fast enough to realistic values  
     parameters.init_refurb_cost_percentage = 0.7;
+    parameters.first_stage_prod_percentage = 0.7;
 
     parameters.MY_value = 350000; %USD, inflation adjusted (and rounded up) from the 1984 number provided in the Koelle paper
     parameters.f1 = 0.6; %from koeller paper
