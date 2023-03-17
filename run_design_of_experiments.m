8579789120313bb471569ff0737eb3f70ec2f143
Asha Jain <ashajain21@utexas.edu>
2023-03-16 21:58:05 -0400

    implemented doe orthogonal arrays

--- run_design_of_experiments.m
+++ run_design_of_experiments.m
@@ -14,24 +14,40 @@ reentry_shield_material_db = readtable("reentry_shield_materials.csv");
 
 
 output_struct_array = zeros([1, num_experiments]);
+output_count = 1;
 
 for i = 1:num_experiments
     %Set up design variables 
-    num_of_launches = experiments{i, 1};
-    reusable_stage_1 = experiments{i, 2};
-    reusable_stage_2 = experiments{i,3};
+    num_of_launches = experiments{i, 2};
     %Engine Prop data 
-    engine_prop_1 = engine_prop_db(experiments{i, 4}, :);
-    engine_prop_2 = engine_prop_db(experiments{i, 5}, :);
-    reentry_shield_material = reentry_shield_material_db(experiments{i, 6}, :);
-    rocket_radius = experiments{i,7};
-    design_variables = setup_designvariables(num_of_launches, reusable_stage_1, reusable_stage_2, engine-prop_1, engine-prop_2, reentry_shield_material, rocket_radius);
+    engine_prop_1 = engine_prop_db(experiments{i, 3}, :);
+    engine_prop_2 = engine_prop_db(experiments{i, 4}, :);
+    reentry_shield_material = reentry_shield_material_db(experiments{i, 5}, :);
+    rocket_radius = experiments{i,6};
+    for j = 1:4
+        if j == 1
+        reusable_stage_1 = 1;
+        reusable_stage_2 = 1;
+        elseif j ==2 
+        reusable_stage_1 = 1;
+        reusable_stage_2 = 0;
+        elseif j ==4 
+        reusable_stage_1 = 0;
+        reusable_stage_2 = 1;
+        else
+        reusable_stage_1 = 0;
+        reusable_stage_2 = 0;
+        end 
+        design_variables = setup_designvariables(num_of_launches, reusable_stage_1, reusable_stage_2, engine-prop_1, engine-prop_2, reentry_shield_material, rocket_radius);
+    
+        [launch_cadence, env_impact, cost] = run_model(design_variables, parameters);
+        Output.launch_cadence = launch_cadence;
+        Output.env_impact = env_impact;
+        Output.cost = cost;
+        output_struct_array(output_count)  = Output;
+        output_count = output_count + 1;
 
-    [launch_cadence, env_impact, cost] = run_model(design_variables, parameters);
-    Output.launch_cadence = launch_cadence;
-    Output.env_impact = env_impact;
-    Output.cost = cost;
-    output_struct_array(i)  = Output;
+    end
 
 end
 
