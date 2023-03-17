72a531f9afd3d4a1cdda7cdb81c87a852c0823d1
Justin <justinpws@gmail.com>
2023-03-17 16:40:11 -0400

    Code adjustments to make top-level scripts run

--- run_design_of_experiments.m
+++ run_design_of_experiments.m
@@ -5,8 +5,10 @@ addpath(genpath(pwd))
 parameters = setup_parameters();
 
 %Load data describing each design experiment 
+doe_bins = readtable("doe_bins.csv");
 experiments = readtable("design_of_experiments.csv");
 num_experiments = size(experiments.Experiment); 
+experiments = format_final_doe_array(num_experiments(1), experiments, doe_bins);
 
 %Loading database data
 engine_prop_db = readtable("engine-prop-combinations.csv");
@@ -21,7 +23,15 @@ for i = 1:num_experiments
     num_of_launches = experiments{i, 2};
     %Engine Prop data 
     engine_prop_1 = engine_prop_db(experiments{i, 3}, :);
+    engine_prop_1.Fuel= string(engine_prop_1.Fuel);
+    engine_prop_1.Engine = string(engine_prop_1.Engine);
+    engine_prop_1.Oxidizer = string(engine_prop_1.Oxidizer);
+    
     engine_prop_2 = engine_prop_db(experiments{i, 4}, :);
+    engine_prop_2.Fuel= string(engine_prop_2.Fuel);
+    engine_prop_2.Engine = string(engine_prop_2.Engine);
+    engine_prop_2.Oxidizer = string(engine_prop_2.Oxidizer);
+
     reentry_shield_material = reentry_shield_material_db(experiments{i, 5}, :);
     rocket_radius = experiments{i,6};
     for j = 1:4
@@ -47,8 +57,20 @@ for i = 1:num_experiments
         output_struct_array(output_count)  = Output;
         output_count = output_count + 1;
 
+        break
+
     end
 
+end
+
+function experiments = format_final_doe_array(num_experiments, experiments, doe_bins)
+   num_factors = 5;
+
+   for exp = 1:num_experiments
+       for factor = 2:num_factors+1
+           experiments(exp, factor) = doe_bins(experiments{exp, factor}, factor);
+       end
+   end
 end
 
 
--- run_model.m
+++ run_model.m
@@ -1,14 +1,17 @@
 function [launch_cadence, env_impact, cost] = run_model(design_variables, parameters)
 
-rocket = struct();
+rocket.stage1.mstruct=parameters.initial_struct_masses(1);
+rocket.stage2.mstruct=parameters.initial_struct_masses(2);
 
 [launch_cadence, rocket] = run_mission_module(design_variables, parameters, rocket);
 
-[rocket] = run_engine_module(design_variables, parameters, rocket);
+[rocket] = engine_mod(rocket, design_variables);
 
-[rocket] = run_aerodynamics_module(design_variables, parameters, rocket);
+rocket.stage1
 
-[rocket] = run_structures_modules(design_variables, parameters, rocket);
+[rocket] = Structures(design_variables, parameters, rocket);
+
+[rocket] = aerodynamics(design_variables, parameters, rocket);
 
 [env_impact] =run_env_impact_module(design_variables, rocket);
 
