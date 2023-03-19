cf32b16f217e2ae3a683ce7df235198c40208861
Asha Jain <ashajain21@utexas.edu>
2023-03-18 20:19:57 -0400

    added effects calculation

--- run_design_of_experiments.m
+++ run_design_of_experiments.m
@@ -6,20 +6,20 @@ parameters = setup_parameters();
 
 %Load data describing each design experiment 
 doe_bins = readtable("doe_bins.csv");
-experiments = readtable("design_of_experiments.csv");
-num_experiments = size(experiments.Experiment); 
-experiments = format_final_doe_array(num_experiments(1), experiments, doe_bins);
+othrogonal_array = readtable("design_of_experiments.csv");
+num_othrogonal_array = size(othrogonal_array.Experiment); 
+
+%Create experiments table 
+experiments = format_final_doe_array(num_othrogonal_array(1), othrogonal_array, doe_bins);
+num_experiments = size(experiments.num_of_launches);
 
 %Loading database data
 engine_prop_db = readtable("engine-prop-combinations.csv");
 reentry_shield_material_db = readtable("reentry_shield_materials.csv");
 
-template_output_struct = struct(launch_cadence=[], env_impact = 0, cost = 0);
-output_struct_array = repmat(template_output_struct, [1, num_experiments]);
-output_count = 1;
 
-for i = 3:num_experiments
-    fprintf('======= Running Experiment %.15g\n', i)
+
+for i = 1:max(num_experiments)
     %Set up design variables 
     num_of_launches = experiments{i, 2};
     %Engine Prop data 
@@ -35,42 +35,68 @@ for i = 3:num_experiments
 
     reentry_shield_material = reentry_shield_material_db(experiments{i, 5}, :);
     rocket_radius = experiments{i,6};
-    for j = 1:4
-        if j == 1
-        reusable_stage_1 = 1;
-        reusable_stage_2 = 1;
-        elseif j ==2 
-        reusable_stage_1 = 1;
-        reusable_stage_2 = 0;
-        elseif j ==4 
-        reusable_stage_1 = 0;
-        reusable_stage_2 = 1;
-        else
-        reusable_stage_1 = 0;
-        reusable_stage_2 = 0;
-        end 
-        design_variables = setup_designvariables(num_of_launches, reusable_stage_1, reusable_stage_2, engine_prop_1, engine_prop_2, reentry_shield_material, rocket_radius);
-    
-        [launch_cadence, env_impact, cost] = run_model(design_variables, parameters);
-        Output.launch_cadence = launch_cadence;
-        Output.env_impact = env_impact;
-        Output.cost = cost;
-        output_struct_array(output_count)  = Output;
-        output_count = output_count + 1;
 
-        break
+    reusable_stage_1 = experiments.reusable_stage1;
+    reusable_stage_2 = experiments.reusable_stage2;
 
-    end
+    design_variables = setup_designvariables(num_of_launches, reusable_stage_1, reusable_stage_2, engine_prop_1, engine_prop_2, reentry_shield_material, rocket_radius);
+
+    [delivery_time, env_impact, cost] = run_model(design_variables, parameters);
+    experiments{i, 8} = sum(cost(1, :));
 
 end
 
-function experiments = format_final_doe_array(num_experiments, experiments, doe_bins)
-   num_factors = 5;
+%Compute the effects of each design variable
+[rows, cols] = size(doe_bins);
+effects = zeros([(rows*cols+4), 1]);
+overall_mean = mean(experiments.output_cost);
+effect_count = 1;
+for k = 1:rows
+    for j = 1:cols
+        factor = doe_bins.Properties.VariableNames{j};
+        factor_value = doe_bins(k,j);
 
+        effects(effect_count) = mean(experiments.output_cost(experiments(:, factor)== factor_value)) - overall_mean;
+        effect_count = effect_count +1;
+    end
+    
+end
+
+for m = 0:1
+
+        effects(effect_count) = mean(experiments.output_cost(experiments(:, "resusable_stage1") == m)) -overall_mean;
+        effects(effect_count+1) = mean(experiments.output_cost(experiments(:, "resusable_stage2") == m)) -overall_mean;
+        effect_count = effect_count +2;
+end 
+
+
+disp(effects);
+
+
+function experiments = format_final_doe_array(num_experiments, orthogonal_array, doe_bins)
+   num_factors = 5;
+   experiments = array2table(zeros(64,8), 'VariableNames',{'num_of_launches', 'engine-prop_1', 'engine-prop_2', 'reentry_shield_material', 'rocket_radius', 'reusable_stage1', 'reusable_stage2', 'output_cost'});
    for exp = 1:num_experiments
        for factor = 2:num_factors+1
-           experiments(exp, factor) = doe_bins(experiments{exp, factor}, factor);
+           experiments(exp, factor-1) = doe_bins(orthogonal_array{exp, factor}, factor);
+           experiments(exp+1, factor-1) = doe_bins(orthogonal_array{exp, factor}, factor);
+           experiments(exp+2, factor-1) = doe_bins(orthogonal_array{exp, factor}, factor);
+           experiments(exp+3, factor-1) = doe_bins(orthogonal_array{exp, factor}, factor);
+
        end
+       experiments{exp, 6} = 1;
+       experiments{exp, 7} = 1;
+       experiments{exp, 8} = 0;
+       experiments{exp+1, 6} = 1;
+       experiments{exp+1, 7} = 0;
+       experiments{exp+1, 8} = 0;
+       experiments{exp+2, 6} = 0;
+       experiments{exp+2, 7} = 1;
+       experiments{exp+2, 8} = 0;
+       experiments{exp+3, 6} = 0;
+       experiments{exp+3, 7} = 0;
+       experiments{exp+3, 8} = 0;
+
    end
 end
 
