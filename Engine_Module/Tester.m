45ebb8f6c9c5b42e67f81944a0e5a4c8a2842402
Kinjal Rücker <kinji@dhcp-10-29-66-33.dyn.MIT.EDU>
2023-03-20 16:45:43 -0400

    adding payload mass

--- Engine_Module/Tester.m
+++ Engine_Module/Tester.m
@@ -6,7 +6,8 @@ design_variables.stage1.engine_prop = {"", "RP-1", "LOX", 337, 3.04E+07, 2.63, 3
 design_variables.stage2.engine_prop = {"", "RP-1", "LOX", 337, 3.04E+07, 2.63, 36.87, 3.8};
 
 rocket.stage1.mstruct = 395700; %kgs
-rocket.stage2.mstruct = 9267; %kgs
+rocket.stage2.mstruct = 9267;   %kgs
+rocket.payload        = 480000; %kgs
 
 design_variables.rocket_ri = 1.5;     %m
 
