7f5e4acb2ffac9595ff5eeb0ab3e83fc5522198c
Kinjal Rücker <kinji@dhcp-10-29-66-33.dyn.MIT.EDU>
2023-03-20 16:36:51 -0400

    adjusted engine count equation

--- Engine_Module/Tester.m
+++ Engine_Module/Tester.m
@@ -2,12 +2,12 @@
 addpath(genpath([pwd '/combustion_toolbox/']))
 
 design_variables.stage1.reusable = 1;
-design_variables.stage1.engine_prop = {"", "RP-1", "LOX", 304, 1.08E+07, 2.36, 21.4, 0.9}; %line
-design_variables.stage2.engine_prop = {"", "RP-1", "LOX", 304, 1.08E+07, 2.36, 21.4, 0.9};
+design_variables.stage1.engine_prop = {"", "RP-1", "LOX", 337, 3.04E+07, 2.63, 36.87, 3.8}; %line
+design_variables.stage2.engine_prop = {"", "RP-1", "LOX", 337, 3.04E+07, 2.63, 36.87, 3.8};
 
-rocket.stage1.mstruct = 10000; %kgs
-rocket.stage2.mstruct = 55000;   %kgs
+rocket.stage1.mstruct = 395700; %kgs
+rocket.stage2.mstruct = 9267; %kgs
 
-design_variables.rocket_ri   = 1.7;     %m
+design_variables.rocket_ri = 1.5;     %m
 
 rocket = engine_mod(rocket, design_variables);
--- Engine_Module/engine_mod.m
+++ Engine_Module/engine_mod.m
@@ -13,7 +13,7 @@ end
 
 rocketProp = getRocketProperties(design_variables.stage2.engine_prop);
 [thrust, ue, mdot, engine] = combustion_mod(rocketProp);
-stage2.nEng         = floor(design_variables.rocket_ri^2/(rocketProp.De^2/4)*.83-1.9);
+stage2.nEng         = floor(design_variables.rocket_ri^2/(rocketProp.De^2/4)*.83);
 stage2.thrust       = thrust*stage2.nEng;
 stage2.ue           = ue    *stage2.nEng;
 stage2.mdot         = mdot  *stage2.nEng;
@@ -24,7 +24,7 @@ stage2.prodValues   = engine.massFraction;
 
 rocketProp = getRocketProperties(design_variables.stage1.engine_prop);
 [thrust, ue, mdot, engine] = combustion_mod(rocketProp);
-stage1.nEng         = floor(design_variables.rocket_ri^2/(rocketProp.De^2/4)*.83-1.9);
+stage1.nEng         = floor(design_variables.rocket_ri^2/(rocketProp.De^2/4)*.83);
 stage1.thrust       = thrust*stage1.nEng;
 stage1.ue           = ue    *stage1.nEng;
 stage1.mdot         = mdot  *stage1.nEng;
