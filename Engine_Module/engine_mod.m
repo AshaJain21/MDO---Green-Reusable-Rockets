9a4b5c6d772e006a74ada8e85af4c4f706226d83
Kinjal Rücker <kinji@dhcp-10-29-89-141.dyn.mit.edu>
2023-03-23 18:57:20 -0400

    rocket eq updated to 10% of structure

--- Engine_Module/engine_mod.m
+++ Engine_Module/engine_mod.m
@@ -4,10 +4,10 @@ stage1 = rocket.stage1;
 stage2 = rocket.stage2;
 
 if design_variables.stage1.reusable == 1
-    delV_stg1 = 2300; % m/s
+    delV_stg1 = 4500; % m/s
     delV_stg2 = 7600; % m/s
 else
-    delV_stg1 = 3400; % m/s
+    delV_stg1 = 5600; % m/s
     delV_stg2 = 7600; % m/s
 end
 
@@ -17,10 +17,11 @@ stage2.nEng         = floor(design_variables.rocket_ri^2/(rocketProp.De^2/4)*.83
 stage2.thrust       = thrust*stage2.nEng;
 stage2.ue           = ue    *stage2.nEng;
 stage2.mdot         = mdot  *stage2.nEng;
-stage2.mf           = stage2.mstruct + rocket.payload;
-% stage2.mi           = stage2.mf*exp((delV_stg2 - delV_stg1)/stage2.ue);
-% stage2.mprop        = stage2.mi - stage2.mf;
-stage2.mprop        = stage2.mf * 1.1;
+
+B                   = exp((delV_stg2 - delV_stg1)/stage2.ue);
+alpha               = 0.1; % 10% of payload mass is structure mass
+stage2.mprop        = (B - 1)*rocket.payload/(1 + alpha - B*alpha);
+
 stage2.prodNames    = engine.name;
 stage2.prodValues   = engine.massFraction;
 
@@ -30,10 +31,11 @@ stage1.nEng         = floor(design_variables.rocket_ri^2/(rocketProp.De^2/4)*.83
 stage1.thrust       = thrust*stage1.nEng;
 stage1.ue           = ue    *stage1.nEng;
 stage1.mdot         = mdot  *stage1.nEng;
-stage1.mf           = stage1.mstruct + stage2.mf + stage2.mprop;
-% stage1.mi           = stage1.mf*exp(delV_stg1/stage1.ue);
-stage1.mprop        = stage1.mf * 1.1;
-% stage1.mprop        = stage1.mi - stage1.mf;
+
+B                   = exp(delV_stg1/stage1.ue);
+alpha               = 0.1; % 10% of payload mass is structure mass
+stage1.mprop        = ((1 + alpha) - B*(1 + alpha))*stage2.mprop/(1 - alpha);
+
 stage1.prodNames    = engine.name;
 stage1.prodValues   = engine.massFraction;
 
