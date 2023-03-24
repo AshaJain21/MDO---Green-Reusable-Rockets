f242e3318bb3df75cdc197d61059d7ef765cbce7
Kinjal Rücker <kinji@dhcp-10-29-89-141.dyn.mit.edu>
2023-03-23 20:20:05 -0400

    added BB mass and initial guesses

--- Engine_Module/Tester.m
+++ Engine_Module/Tester.m
@@ -4,13 +4,17 @@ addpath(genpath([pwd '/combustion_toolbox/']))
 design_variables.stage1.reusable = 1;
 design_variables.stage1.engine_prop = {"", "RP-1", "LOX", 337.5, 1.8E+07, 2.6, 35, 1.338}; %line
 design_variables.stage2.engine_prop = {"", "RP-1", "LOX", 337.5, 1.8E+07, 2.6, 35, 1.338};
+design_variables.rocket_ri = 1.5; % m
 
-rocket.stage1.mstruct = 22000; %kgs
-rocket.stage2.mstruct = 4000;   %kgs
-rocket.payload        = 20000; %kgs
-stage1.alpha          = 0.1; % 10% of payload mass is structure mass
-stage2.alpha          = 0.1; % 10% of payload mass is structure mass
+rocket.stage1.mstruct = 22000; % kgs
+rocket.stage2.mstruct = 4000;  % kgs
+rocket.payload        = 20000; % kgs
+rocket.stage1.alpha   = 0.1;   % 10% of payload mass is structure mass
+rocket.stage2.alpha   = 0.1;   % 10% of payload mass is structure mass
 
-design_variables.rocket_ri = 1.5;     %m
+parameters.struc_to_propellant_mass_ratio = .1;
+parameters.vTerm1 = 53;
+parameters.vTerm2 = 53;
 
-rocket = engine_mod(rocket, design_variables);
+rocket.iter = 1;
+rocket = engine_mod(rocket, design_variables, parameters);
--- Engine_Module/engine_mod.m
+++ Engine_Module/engine_mod.m
@@ -1,7 +1,10 @@
-function [rocket] = engine_mod(rocket, design_variables)
+function [rocket] = engine_mod(rocket, design_variables, parameters)
 
 stage1 = rocket.stage1;
 stage2 = rocket.stage2;
+alpha  = parameters.struc_to_propellant_mass_ratio;
+vTerm1 = parameters.vTerm1;
+vTerm2 = parameters.vTerm2;
 
 if design_variables.stage1.reusable == 1
     delV_stg1 = 4500; % m/s
@@ -17,9 +20,13 @@ stage2.nEng         = floor(design_variables.rocket_ri^2/(rocketProp.De^2/4)*.83
 stage2.thrust       = thrust*stage2.nEng;
 stage2.ue           = ue    *stage2.nEng;
 stage2.mdot         = mdot  *stage2.nEng;
-
 B                   = exp((delV_stg2 - delV_stg1)/stage2.ue);
-stage2.mprop        = (B - 1)*rocket.payload/(1 + (1 - B)*stage2.alpha);
+if rocket.iter == 1
+    stage2.mprop    = (B - 1)*rocket.payload/(1 + (1 - B)*alpha);
+    stage2.mBB      = (exp(vTerm2/stage2.ue) - 1)*alpha*stage2.mprop;
+else
+    stage2.mprop    = (B - 1)*(stage2.mstruct + rocket.payload);   
+end
 
 stage2.prodNames    = engine.name;
 stage2.prodValues   = engine.massFraction;
@@ -32,8 +39,13 @@ stage1.ue           = ue    *stage1.nEng;
 stage1.mdot         = mdot  *stage1.nEng;
 
 B                   = exp(delV_stg1/stage1.ue);
-stage1.mprop        = (1 - B)*(1 + stage1.alpha)*stage2.mprop/(1 - stage1.alpha);
-
+if rocket.iter == 1
+    stage1.mprop    = (1 - B)*(1 + alpha)*stage2.mprop/(1 - alpha);
+    stage1.mBB      = (exp(vTerm1/stage1.ue) - 1)*alpha*stage1.mprop;
+else
+    mf              = stage1.mstruct + rocket.payload + stage2.mstruct + stage2.mprop;
+    stage1.mprop    = (B - 1)*mf;
+end
 stage1.prodNames    = engine.name;
 stage1.prodValues   = engine.massFraction;
 
