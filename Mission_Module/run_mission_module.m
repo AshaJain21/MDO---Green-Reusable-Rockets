e6988dfa32f69533775d83213db39af5f65f9d28
Justin <justinpws@gmail.com>
2023-03-31 13:11:41 -0400

    Minor update to include payload configuration information in the rocket struct

--- Mission_Module/run_mission_module.m
+++ Mission_Module/run_mission_module.m
@@ -9,8 +9,8 @@ function [launch_cadences, rocket] = run_mission_module(design_variables, parame
     rocket.payload = per_launch_mass;
 
     %CALCULATE PAYLOAD HEIGHT
-    num_sat_stacks = calculate_num_stacks(design_variables.rocket_ri, parameters.sat_model_radius, parameters.circle_packing_table.Density);
-    num_sats_per_stack = ceil(num_sat_per_launch / num_sat_stacks);
+    rocket.num_sat_stacks = calculate_num_stacks(design_variables.rocket_ri, parameters.sat_model_radius, parameters.circle_packing_table.Density);
+    rocket.num_sats_per_stack = ceil(num_sat_per_launch / num_sat_stacks);
     rocket.payload_height = num_sats_per_stack * parameters.sat_model_height;
 
     %SET UP LEARNING CURVE FOR SATELLITE PRODUCTION TIMES
