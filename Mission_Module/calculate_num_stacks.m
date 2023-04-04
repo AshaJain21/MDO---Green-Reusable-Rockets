af6f3bfbcd10f02780a333008fe1e71aa8a542d8
Justin <justinpws@gmail.com>
2023-04-04 16:10:18 -0400

    Removing old sat packing function

--- Mission_Module/calculate_num_stacks.m
+++
@@ -1,9 +0,0 @@
-function num_sat_stacks = calculate_num_stacks(rocket_ri, sat_ri, circle_packing_densities)
-    for i = 1:length(circle_packing_densities)
-        density = (i*pi*sat_ri^2)/(pi*rocket_ri^2);
-        if density > circle_packing_densities(i)
-            num_sat_stacks = i-1;
-            break
-        end
-    end
-end
