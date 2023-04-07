1a288eafbc1572c065ae770b1ff2bebeabd4e61c
Justin <justinpws@gmail.com>
2023-04-07 10:11:23 -0400

    Updated number of engine-prop combinations to 15

--- run_ga.m
+++ run_ga.m
@@ -3,7 +3,7 @@ addpath(genpath(pwd))
 problem.fitnessfcn = @run_model_ga;
 problem.nvars = 9;
 problem.lb = [12, 0, 0, 1,1,1,0.8,7000, 1000]; 
-problem.ub = [1000, 1, 1, 17,17,11,4.5,4e6, 1.5e6];
+problem.ub = [1000, 1, 1, 15,15,11,4.5,4e6, 1.5e6];
 problem.solver = 'ga';
 options = optimoptions('ga');
 problem.options = options;
