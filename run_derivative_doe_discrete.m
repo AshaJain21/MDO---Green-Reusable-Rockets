73252d59d384fb6347d32724538e593cc7ae17e5
Maranda Cherry <17mfc3@queensu.ca>
2023-04-09 19:11:16 -0400

    updated fmincon fxn in run_derivative

--- run_derivative_doe_discrete.m
+++ run_derivative_doe_discrete.m
@@ -1,3 +1,4 @@
+%%run derivative engine mod
 clc;clear;
 addpath(genpath(pwd))
 problem.nvars = 3;
@@ -5,9 +6,8 @@ problem.nvars = 3;
 problem.lb = [0.8,   7000,  1000]; 
 problem.ub = [4.5,   4e6,   1.5e6];
 problem.solver = 'fmincon';
-options = optimoptions('fmincon','Display',...
-    'iter','Algorithm','sqp', 'MaxFunctionEvaluations', 3000);
+options = optimoptions('fmincon','Display','iter','Algorithm','sqp');
 problem.options = options;
 problem.objective = @run_model_derivative;
-problem.x0 = [3.0305	1505000	1500000];%[1.561, 1.0774e6, 1.6552e5];
+problem.x0 = [3.0305	10000	755000];%[1.561, 1.0774e6, 1.6552e5];
 [x,fval,exitflag,output,lambda,grad,hessian]  = fmincon(problem);
