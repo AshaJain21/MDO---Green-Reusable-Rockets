617ad8148cbb21d6ebf411a94611b91aa335b64a
Maranda Cherry <17mfc3@queensu.ca>
2023-04-11 14:03:35 -0400

    modified derivative run

--- run_model_derivative.m
+++ run_model_derivative.m
@@ -1,7 +1,7 @@
 function penalized_cost = run_model_derivative(x)
     %Set discrete design variables 
     %# launch,reuse1,reuse2,engine1,engine2,re-entry mat
-    num_launches = 45;
+    num_launches = 800;
     stage1_boolean = 0;
     stage2_boolean = 0;
     engine_prop_1_row = 4;
@@ -15,7 +15,7 @@ function penalized_cost = run_model_derivative(x)
     [~, ~, ~, ~, cost, constraints] = run_model(design_variables, parameters);
     [g, h] = calculate_penalties(constraints);
     penalized_cost  = sum(cost(1, :)) + g + h;
-
+    
 end
 
 function [g, h] = calculate_penalties(constraints)
@@ -23,12 +23,20 @@ function [g, h] = calculate_penalties(constraints)
    h = 0;
     %Inequality Constraints 
     g = g + max(0, constraints.launch_cadence);
+    g1= max(0, constraints.launch_cadence);
     g = g + max(0, constraints.max_cost_year);
+    g2 = max(0, constraints.launch_cadence);
     g = g + max(0, constraints.rocket_height);
+    g3 =max(0, constraints.rocket_height);
     g = g + max(0, constraints.payload_height);
+    g4 = max(0, constraints.payload_height);
     g = g + max(0, constraints.min_stg1_num_engines);
+    g5 = max(0, constraints.min_stg1_num_engines);
     g = g + max(0, constraints.max_stg1_num_engines);
-
+    g6 = max(0, constraints.max_stg1_num_engines);
+           %[launch cad, cost/yr, rockh, payh, minst1_eng,maxst1_eng]
+    gvals = [g1,            g2,     g3,   g4,   g5,         g6];
+    %dlmwrite('gvals2_3.dat',gvals, '-append');
     %Equality Constraints 
     h = h + (constraints.mprop1)^2;
     h = h + (constraints.mprop2) ^2;
