4723c763d3c069fa9afa58014514bdad26b9eb58
Justin <justinpws@gmail.com>
2023-04-14 18:20:28 -0400

    updated how run_model_ga calculates the penalty function

--- run_model_ga.m
+++ run_model_ga.m
@@ -28,20 +28,38 @@ function [g, h] = calculate_penalties(constraints, parameters, rocket, design_va
    g = 0;
    h = 0;
     %Inequality Constraints 
-    g = g + (max(0, constraints.launch_cadence)/(parameters.delivery_time * 12));
-    g = g + (max(0, constraints.max_cost_year)/parameters.max_cost_per_year);
-    g = g + (max(0, constraints.rocket_height)/parameters.max_rocket_height);
-    
-    total_rocket_height = rocket.stage1.height + rocket.stage2.height;
-    g = g + (max(0, constraints.payload_height)/(parameters.max_payload_height_fraction*total_rocket_height));
-
-    g = g + (max(0, constraints.min_stg1_num_engines)/parameters.min_num_engines);
-    g = g + (max(0, constraints.max_stg1_num_engines)/parameters.max_num_engines);
-    g = g + (max(0, constraints.eng_stg1_rocket_size)/design_variables.rocket_ri);
-    g = g + (max(0, constraints.eng_stg2_rocket_size)/design_variables.rocket_ri);
+%     g = g + (max(0, constraints.launch_cadence)/(parameters.delivery_time * 12));
+%     g = g + (max(0, constraints.max_cost_year)/parameters.max_cost_per_year);
+%     g = g + (max(0, constraints.rocket_height)/parameters.max_rocket_height);
+%     
+%     total_rocket_height = rocket.stage1.height + rocket.stage2.height;
+%     g = g + (max(0, constraints.payload_height)/(parameters.max_payload_height_fraction*total_rocket_height));
+% 
+%     g = g + (max(0, constraints.min_stg1_num_engines)/parameters.min_num_engines);
+%     g = g + (max(0, constraints.max_stg1_num_engines)/parameters.max_num_engines);
+%     g = g + (max(0, constraints.eng_stg1_rocket_size)/design_variables.rocket_ri);
+%     g = g + (max(0, constraints.eng_stg2_rocket_size)/design_variables.rocket_ri);
+% 
+%     g = g + (max(0, constraints.mprop1)/design_variables.mprop1_guess);
+%     g = g + (max(0, constraints.mprop2)/design_variables.mprop2_guess);
 
     %Equality Constraints 
-    h = h + (constraints.mprop1/rocket.stage1.mprop)^2;
-    h = h + (constraints.mprop2/rocket.stage1.mprop) ^2;
+    
+
+
+    g = (max(0, constraints.launch_cadence)/(parameters.delivery_time * 12));
+    g = (max(0, constraints.max_cost_year)/parameters.max_cost_per_year);
+    g = (max(0, constraints.rocket_height)/parameters.max_rocket_height);
+    
+    total_rocket_height = rocket.stage1.height + rocket.stage2.height;
+    g = (max(0, constraints.payload_height)/(parameters.max_payload_height_fraction*total_rocket_height));
+
+    g = (max(0, constraints.min_stg1_num_engines)/parameters.min_num_engines);
+    g = (max(0, constraints.max_stg1_num_engines)/parameters.max_num_engines);
+    g = (max(0, constraints.eng_stg1_rocket_size)/design_variables.rocket_ri);
+    g = (max(0, constraints.eng_stg2_rocket_size)/design_variables.rocket_ri);
+
+    g = (max(0, constraints.mprop1)/design_variables.mprop1_guess);
+    g = (max(0, constraints.mprop2)/design_variables.mprop2_guess);
 
 end
