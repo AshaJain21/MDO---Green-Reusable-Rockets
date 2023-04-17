e7fd5c7cb6176fa1d76a70bdfb5affc6bbe41e86
Maranda Cherry <17mfc3@queensu.ca>
2023-04-17 14:27:20 -0400

    Update run derivative

--- run_derivative.m
+++ run_derivative.m
@@ -19,23 +19,27 @@ problem.objective = @run_model_derivative;
 problem.x0 = [1.561	1505000	755000 ];%[1.561, 1.0774e6, 1.6552e5];
 [x,fval,exitflag,output,lambda,grad,hessian]  = fmincon(problem);
 
-numlaunch = [20 45 410 800];
-%engine1 = 1:1:15;
-%engine2 = 1:1:15;
+%numlaunch = [20 45 410 800];
+%engine1 = 1:1:15; %[1, 5, 7, 9, 11,12, 15]
+%engine2 = 1:1:15; %1, 5, 7, 9, 11, 12, 15
 %reuse1 = [0 1];
 %reuse2 = [0 1];
 %rematmate = 1:1:9;
-
-ri = [1.561, 3.0305, 4.5];
-st1mprop = [1e4, 1505000, 3e6];
-st2mprop = [1e4, 755000, 1.5e6];
+%% 
+ri = [1.561,4.5];% 3.0305, 4.5];
+st1mprop = [1e4,3e6];% 1505000, 3e6];
+st2mprop = [1e4, 1.5e6];%755000, 1.5e6];
 
 num_trials = length(ri) * length(st1mprop)*length(st2mprop);
 %Initialize arrays for results
-doe_results = zeros(num_trials, 7);%(2+problem.nvars+3));
+doe_results = zeros(num_trials, 5);%(2+problem.nvars+3));
+%doe_flag = zeros(num_trials, 7);%(2+problem.nvars+3));
 exp_num = 1;
-output_full = struct(problemtype='', rngstate=struct(), generations=0, funccount=0, message='', maxconstraint=0, hybridflag=[]);
-population_data_full = zeros(1, (problem.nvars+1));
+output_full = struct(iterations=0, funcCount=0, algorithm = '', message='',...
+    constrviolation=0, stepsize=0, lssteplength=0, firstorderopt=0,bestfeasible=[]);
+grad_data = zeros(3, (num_trials));
+lamb_data =struct(eqlin=[], eqnonlin=[], ineqlin=[], ineqnonlin=[], lower=[], upper=[]);%zeros(1, (problem.nvars+1));
+hess_data = zeros(3, (problem.nvars*num_trials));
 %x,fval,exitflag,output,lambda,grad,hessian
 
 for i = 1:length(ri)
@@ -48,7 +52,7 @@ for i = 1:length(ri)
 
             %options = optimoptions('ga', 'PopulationSize', pop_size, 'MutationFcn', mutation_settings);
             options = optimoptions('fmincon','OutputFcn',@savemilpsolutions,'Display',...
-                         'iter','Algorithm','sqp', 'MaxFunctionEvaluations', 3000);
+                         'iter','Algorithm','sqp', 'MaxFunctionEvaluations', 3000, 'ConstraintTolerance', 1e-1);
             problem.options = options;
             problem.objective = @run_model_derivative;
             fprintf('Rocket Inner Radius: %f, St1 Prop Mass: %f, St2 Prop Mass: %f \n', ri_trial, prop1, prop2);
@@ -59,10 +63,14 @@ for i = 1:length(ri)
             %[x_opt,fval,exitflag,output,population,scores] 
             %[pop_size, mutation_rate, x_opt, exitflag, fval, comp_time];
             % Store Values
-            doe_results(exp_num, :) = [x,fval,exitflag,output,lambda,grad,hessian];
+            doe_results(exp_num, :) = [x, fval, exitflag];%,output,lambda,grad,hessian];
+            %doe_flag(exp_num, :) = [fval,exitflag];
             marker_row = ones(1, (problem.nvars+1))*exp_num;
-            population_data_full = [population_data_full; marker_row;[population, scores]];
+            %gradhess_data = [lambda,grad,hessian]; %marker_row;[population, scores]];
             output_full = [output_full, output];
+            grad_data(:,exp_num) =grad; 
+            lamb_data = [lamb_data, lambda];
+            hess_data(:,i:i+2) = hessian;
             exp_num = exp_num + 1;
         
         end
