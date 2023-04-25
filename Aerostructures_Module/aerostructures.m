4a9434fbf52a6ce63581acda55e366c55a3b7ed3
Maranda Cherry <17mfc3@queensu.ca>
2023-04-25 14:04:27 -0400

    updated aerostruct

--- Aerostructures_Module/aerostructures.m
+++ Aerostructures_Module/aerostructures.m
@@ -14,21 +14,26 @@ g = 9.81; %m/s2
 ri = design_variables.rocket_ri; %rocket inner radius
 st1prop = design_variables.mprop1_guess; %initial prop mass guess
 st2prop = design_variables.mprop2_guess; %init prop mass guess stage 2
-t1 = design_variables.stage1.engine_prop{1,10};
+t1 = design_variables.stage1.engine_prop{1,10}; %thrust
 t2 = design_variables.stage2.engine_prop{1,10};
-DE1 =design_variables.stage1.engine_prop{1,8};
+DE1 =design_variables.stage1.engine_prop{1,8}; %engine exit diameter
 DE2 =design_variables.stage2.engine_prop{1,8};
 rocket.stage1.nEng = calc_inscribed_circles(DE1/2, ri);
+stg1_neng = rocket.stage1.nEng;
 rocket.stage1.thrust = t1*rocket.stage1.nEng;
 thrust1 = rocket.stage1.thrust;
 rocket.stage2.nEng = calc_inscribed_circles(DE2/2, ri);
 rocket.stage2.thrust = t2*rocket.stage2.nEng;
 thrust2 = rocket.stage2.thrust;
+stg2_neng = rocket.stage2.nEng;
 reuse1 = design_variables.stage1.reusable;
 reuse2 = design_variables.stage2.reusable;
-mdotst1_f = thrust1/(g*design_variables.stage1.engine_prop{1,4}); %mass flow rate of exhaust 
-mdotst2_f = thrust2/(g*design_variables.stage2.engine_prop{1,4}); %mass flow rate of exhaust upper stage
+mass_eng_st1 = stg1_neng*design_variables.stage1.engine_prop{1,9}; %mass of engines
+mass_eng_st2 = stg2_neng*design_variables.stage2.engine_prop{1,9}; %mass of engines
 %mdot = f/(isp*g)= mdot
+st1ve = g*design_variables.stage1.engine_prop{1,4}; %effective exhaust ve
+st2ve = g*design_variables.stage2.engine_prop{1,4};
+
 payh = rocket.payload_height; %get volume of satellites available 
 re_mat_density = design_variables.stage2.reentry_shield_material.Density; %[kg/m3] 
 strucmat_density = parameters.structural_material.density; %[kg/m3]
@@ -92,23 +97,21 @@ h2 = stgsep:1000:LEOalt; %altitude array for stage 2 75km to LEO 500 km
 %mx^2 + bx + c c=0 at t=0, dy/dx  
 %theta_l = 0; %launch angle - parabolic y = x^2
 %rocket_Sb_launch = pi*ro*2*st2h/cos(theta_l) + pi*ro*2*st1h/cos(theta_l);
-st1ve = thrust1/mdotst1_f;                                                  
+mdotst1_f = thrust1/st1ve; %total thrust for all engines
+%st1ve = thrust1/mdotst1_f;                                                  
 fc = mdotst1_f/st1ve;  %fuel consumption kg per metre; 
 
-%Stage 2
-%theta_stg2 = 0; %launch angle - parabolic y = x^2 still going up
-%stg2_Sb_sep = pi*ro*2*st2h/cos(theta_l); 
-st2ve = thrust2/mdotst2_f;
+mdotst2_f = thrust2/st2ve; %total thrust for all engines
 fc2 = mdotst2_f/st2ve;  %fuel consumption kg per metre; 
 
 %Run Aero Launch function (Full Stage 1 and 2 launch to LEO)
-[maxq] = launch(h, h2, ro,fc, g, fc2, st1mass,st1prop,st2prop,st2mass...
-    ,pay_mass,heat_shield_mass, thrust1, thrust2);
+[maxq] = launch(h, h2,ro,fc, fc2, st1mass,st1prop,st2prop,st2mass,...
+    pay_mass,heat_shield_mass,mass_eng_st1,mass_eng_st2, st1ve, st2ve);
 
 %Check max q ensure wall thickness is enough
 %Constraint on wall thickness for max q
 %q = stress FOS*thrust/sigma_max = area needed
- 
+
 if maxq > sigma_max
     %fprintf(['Wall thickness for rocket'...                 
      %   'is not enough to support max q in flight']);
@@ -122,15 +125,6 @@ if maxq > sigma_max
 % else 
 %     fprintf('Wall Thickness Supports Max Q');
 end 
-%check again using new masses
-%[maxqnew] = launch(h, h2, ro,fc, g, fc2, st1mass,st1prop,st2prop,...
-%    st2mass,pay_mass,heat_shield_mass, thrust1, thrust2);
-
-%if maxqnew > maxq
- %   fprintf('New Maxq is larger...');
-% else 
-%     fprintf('Wall Thickness Supports Max Q');  
-%end 
 
 
 %% Calculate FINAL Structural masses
@@ -139,8 +133,11 @@ rocket.ro = ro;
 vol = pi*(ro^2 -ri^2)*st1h;
 rocket.stage1.mstruct =vol*strucmat_density;
 
+rocket.stage1.mass_eng = mass_eng_st1;
+%ROCKET ENGINE MASS
 vol2 = pi*(ro^2 -ri^2)*st2h;
 rocket.stage2.mstruct =vol2*strucmat_density;
+rocket.stage2.mass_eng = mass_eng_st2;
 
 rocket.stage2.heat_shield_SA = SAst2; %[m2] surface area of the heat shield required
 rocket.stage2.heatshield_mass = heat_shield_mass;
@@ -170,22 +167,30 @@ if reuse2 == 1 %re-entry, landing burn needed + belly flop
     %cross sectional area at angle of fall
     st2_cross_recovery = pi*ro^2/cos(deg2rad(reangle)); 
     st2_Sb_recovery = ro*2*st2h/cos(deg2rad(reangle)); %total wetted area not sure which to use!!!!!!!!!!!!!!!!!!!!!!
-    alt = 500:500:LEOalt; %full altitude of deorbit [m]
+    alt = 500:2000:LEOalt; %full altitude of deorbit [m]
     st2tv = zeros(1,length(alt));
     ust2 = zeros(1,length(alt));
     ust2(1) = 7.2e3; %assume a deorbit velocity of ~7.2 km/s from staging paper
-    
+    q = zeros(1,length(alt)); %rocket dynamic pressure
+    %[~,~,~, rho1] = atmoscoesa(LEOalt, 'None');
+    q(1) = 0; %neglect air drag here
+%time = zeros(1, (length(h) + length(h2))-1) ;%per m total time for reaching LEO
+%accel = zeros(1,length(h));
     for i = 2:(length(alt)) 
-     [~,~,~, rho] = atmoscoesa(alt(end-i+1), 'None'); %start at altitude of sep.
-        %delv = 2*g*500; %projectile motion NOT SURE IF THIS APPLIES 
        if alt(end-i+1) > 84852
-            [~,~,~, rho] = atmoscoesa(84852); %change this later         
+           rho =0; %neglect drag
+            %[~,~,~, rho] = atmoscoesa(84852); %change this later
+       else
+            [~,~,~, rho] = atmoscoesa(alt(end-i+1), 'None'); %start at altitude of sep.
        end
        Cd = 1.17; %FOR NOW
-        a = (- g) + (0.5*rho*Cd*ust2(i)^2)/st2mpb; %calculate velocity of rocket (downwards)
-        ust2(i) = ust2(i-1) + 2*a*500;
-        
-        st2tv(i) = sqrt(2*st2mpb*g/rho*st2_Sb_recovery*(Cd)); %goes from high alt to end
+        %[~,~,~,rho] = atmoscoesa(h(i), 'None');
+       D = q(i-1)*st2_Sb_recovery*Cd; %CD cylinder
+       accel = D/st2mpb - g; %drag and gravity
+       ust2(i) =sqrt( (ust2(i-1))^2 + 2*accel*2000); %500 m
+       q(i) = 0.5*rho*ust2(i)^2; %dynamic pressure
+       %need final velocity 500 m above ground
+       st2tv(i) = sqrt(2*st2mpb*g/rho*st2_Sb_recovery*(Cd)); %goes from high alt to end
     end
       rocket.stage2.terminal_velocity = st2tv(end); %IMPLEMENT TOTAL DRY MASS + NOT TERMINAL VELOCITY
 end
@@ -254,14 +259,6 @@ st1mass =vol*strucmat_density; %initial rocket stage 1 mass
 vol2 = pi*(ro^2 -ri^2)*st2h;
 st2mass =vol2*strucmat_density; %initial rocket stage 2 mass
 
-% %% Check constraints -ensure not too high
-% %ensure it is not too high
-% if (st1h+st2h) > 100 %[m]
-%     rocket.checkheight = 0; %pass to constraint module, optimizer disregard this run
-%     fprintf('Rocket Exceeds height constraint of 100m')
-% else 
-%     rocket.checkheight =1; %module will continue
-% end
 
 %% Size shield thickness via max heat flux
 %its probably ~4mm so this estimate may be too thick for now
@@ -274,16 +271,18 @@ vol_heatshield = SAst2*reshieldthick;
 heat_shield_mass = vol_heatshield*re_mat_density; %[kg]
 
 end
-function [maxq] = launch(h, h2,ro,fc, g, fc2, st1mass,st1prop,st2prop,st2mass,pay_mass,heat_shield_mass, thrust1, thrust2)
+function [maxq] = launch(h, h2,ro,fc, fc2, st1mass,st1prop,st2prop,st2mass,...
+    pay_mass,heat_shield_mass,mass_eng_st1,mass_eng_st2, st1ve, st2ve)
 %% Launch
-totalmass = st1mass + st1prop + st2prop + st2mass + pay_mass + heat_shield_mass;
+totalmass = st1mass + st1prop + st2prop + st2mass + pay_mass + heat_shield_mass...
+    + mass_eng_st1 + mass_eng_st2;
 rocket_SA = pi*ro^2;
 q = zeros(1,length(h)); %rocket dynamic pressure
 rockmass = zeros(1,length(h)); %array of rocket mass
 u = zeros(1,length(h)); %array of rocket mass
 rockmass(1) = totalmass; %CHECK THAT I SHOULD DO THIS 1:2 OR IF i SHOULD DO IT JUST TO 1 !!!!
 time = zeros(1, (length(h) + length(h2))-1) ;%per m total time for reaching LEO
-accel = zeros(1,length(h));
+D = zeros(1,length(h));
  for i = 2:length(h) %%%%%%DO I NEED TO DO H-1 IN LENGTH TO ENSURE ROCKET MASS DOES NOT GO OUTSIDE OF ARRAY BOUNDS 
      %get atmospheric data
         [~,~,~,rho] = atmoscoesa(h(i), 'None');
@@ -291,10 +290,11 @@ accel = zeros(1,length(h));
         mi = rockmass(i-1); %initial fuel
         mf = rockmass(i-1) - fc*500; %updates fuel consumption for every 500 m
         rockmass(i) = mf; %update new mass
-        %delv = -st1ve*log(mf/mi); %ideal rocket equation change in vel IS THIS MASS THE ROCKET MASS OR THE STAGE 1 MASS?
-        D = q(i-1)*rocket_SA*1.17; %CD cylinder
-        accel(i) = (thrust1 - D)/mi - g; %drag and gravity
-        u(i) = u(i-1) + 2*accel(i); %500 m
+        delv = -st1ve*log(mf/mi); %ideal rocket equation change in vel IS THIS MASS THE ROCKET MASS OR THE STAGE 1 MASS?
+        u(i) = u(i-1) + delv; %2*accel(i)*500); %500 m
+        %D(i) = q(i-1)*rocket_SA*1.17; %CD cylinder
+        %accel(i) = (thrust1 - D - g*mi)/mi; %drag and gravity
+        %u(i) =sqrt( (u(i-1))^2 + 2*accel(i)*500); %500 m
         time(i) = 2/( u(i) + u(i-1) ) ; %basic kinematics
         q(i) = 0.5*rho*u(i)^2; %dynamic pressure
    
@@ -308,21 +308,21 @@ u2(1) = u(end); %from above
 q2 = zeros(1,length(h2)); %rocket dynamic pressure
 st2mass_calc = zeros(1,length(h2)); %array of stg2 mass
 st2mass_calc(1) = st2mass+st2prop; %starting stage mass 
-accel2 = zeros(1,length(h2));
+
  for i = 2:length(h2)
      %get atmospheric data
       [~,~,~,rho] = atmoscoesa(h2(i), 'None');
       mi = st2mass_calc(i-1); %initial fuel
       mf = st2mass_calc(i-1) - fc2*1000; %updates fuel consumption for every 1000 m
       st2mass_calc(i) = mf; %update new mass
-      D = q2(i-1)*rocket_SA*1.17; %CD cylinder
-      accel2(i) = (thrust2 -D)/mi -g;%drag and gravity
-      u2(i) = u2(i-1) + 2*accel2(i); %1000 m
+      %D = q2(i-1)*rocket_SA*1.17; %CD cylinder
+      delv = -st2ve*log(mf/mi); %ideal rocket equation change in vel IS TH
+      u2(i) =(u2(i-1)) + delv; %500 m
       time(i +length(h)) = 2/(u2(i) + u2(i-1) ) ; %basic kinematics CHECK CONTINUITY HEREEEEEE!!!! There mauy be a gap in time
       q2(i) = 0.5*rho*u2(i)^2; %dynamic pressure
  end
   
-maxq2 = max(q2);
+maxq2 = abs(max(q2));
 maxq = [maxq1, maxq2];
 if max(maxq) < 30397.5
     maxq = 30397.5;
