c96b4b992e812d17e864a0a64feda0112b95e340
Justin <justinpws@gmail.com>
2023-05-01 23:42:14 -0400

    Modified plot_pareto to use self-written pareto filtering function

--- plot_pareto.m
+++ plot_pareto.m
@@ -4,20 +4,30 @@ addpath(genpath(pwd))
 
 trial_num = 1;
 selected_pt_num = 2;
+use_optimal_points_all_trials = 1;
+data_file_1 = 'ga_multiobj_run6.mat';
+data_file_2 = 'ga_multiobj_run7.mat';
 
-load('ga_multiobj_run6.mat')
+load(data_file_1);
 combined_population = mdo_proj_populations;
 objective_vals_unsorted = doe_res(trial_num).fval;
 x_opt_combined = doe_res(trial_num).x_opt;
-load('ga_multiobj_run7.mat', "mdo_proj_populations", "doe_res");
+
+if use_optimal_points_all_trials == 1
+    load(data_file_2, "mdo_proj_populations", "doe_res");
+    objective_vals_unsorted = [objective_vals_unsorted; doe_res(trial_num).fval];
+    x_opt_combined = [x_opt_combined; doe_res(trial_num).x_opt];
+    pareto_points = [x_opt_combined, objective_vals_unsorted];
+else
+    load(data_file_2, "mdo_proj_populations");
+    xopt = x_opt_combined;
+    pareto_points = [doe_res(trial_num).x_opt, doe_res(trial_num).fval];
+end
+
 combined_population = [combined_population;mdo_proj_populations];
-objective_vals_unsorted = [objective_vals_unsorted; doe_res(trial_num).fval];
-x_opt_combined = [x_opt_combined; doe_res(trial_num).x_opt];
 
 objective_vals_unsorted(:,2) = objective_vals_unsorted(:,2)./1e5;
 objective_vals_unsorted(:,3) = objective_vals_unsorted(:,3)*1e9;
-
-pareto_points_combined = [x_opt_combined, objective_vals_unsorted];
 
 population_scores = doe_res(trial_num).scores;
 
@@ -27,21 +37,17 @@ filtered_populations = filtered_populations(:, 1:end-1); %This line removes the 
 filtered_populations(:, 11) = filtered_populations(:, 11)./1e4;
 filtered_populations(:, 12) = filtered_populations(:, 12)*1e9;
 
-pareto_points_unique = unique(pareto_points_combined, 'rows');
-xopt = pareto_points_unique(:, 1:9);
-objective_vals_unsorted = pareto_points_unique(:, 10:end);
-
-[objective_vals_unsorted, pareto_point_idxs] = paretoFront( objective_vals_unsorted );
-
-xopt = xopt(pareto_point_idxs, :);
+if use_optimal_points_all_trials == 1
+    [objective_vals_unsorted, pareto_point_idxs] = paretoFiltering(filtered_populations);
+    xopt = filtered_populations(pareto_point_idxs, 1:9);
+end
 
 % Limit how much of the dominated designs are shown
-% od_lim = 0.15;
-% cost_lim = 7e10;
-% filtered_populations = filtered_populations( (filtered_populations(:,11) <= od_lim) & (filtered_populations(:, 12) <= cost_lim), :);
+od_lim = 0.15;
+cost_lim = 7e10;
+filtered_populations = filtered_populations( (filtered_populations(:,11) <= od_lim) & (filtered_populations(:, 12) <= cost_lim), :);
 
 %% Computing rocket, launch cadence charateristics for pareto front solutions
-pareto_points = [doe_res(trial_num).x_opt, doe_res(trial_num).fval];
 pareto_points(:, 11) = pareto_points(:, 11)./1e4;
 pareto_points(:, 12) = pareto_points(:, 12)*1e9;
 
@@ -66,7 +72,7 @@ engine_prop_db = readtable("engine-prop-combinations.csv");
 reentry_shield_material_db = readtable("reentry_shield_materials.csv");
 pareto_solution_outputs = [];
 
-for pareto_solu = 1:length(xopt)
+for pareto_solu = 1:height(xopt)
     x = xopt(pareto_solu, :);
     engine_prop_1_row = round(x(4));
     engine_prop_2_row = round(x(5));
@@ -84,8 +90,8 @@ ax1 = nexttile;
 objective_vals = sortrows(objective_vals_unsorted, [1, 2, 3]);
 plot3(objective_vals(:,1), objective_vals(:,2), objective_vals(:,3), '.-', 'MarkerSize', 20)
 grid on
-set(ax1,'Xscale','log','Zscale','log','Yscale','log')
-xlabel('Radiative Forcing [W/m^2]', 'FontSize', 14)
+% set(ax1,'Xscale','log','Zscale','log','Yscale','log')
+xlabel('Radiative Forcing [mW/m^2]', 'FontSize', 14)
 ylabel('Ozone Depletion [%]', 'FontSize', 14)
 zlabel('Cost [$]', 'FontSize', 14)
 title('Plot of Pareto Points Only', 'FontSize', 16)
@@ -97,7 +103,8 @@ hold on
 scatter3(filtered_populations(:,10), filtered_populations(:,11), filtered_populations(:,12), 100, 'r.')
 hold off
 grid on
-xlabel('Radiative Forcing [W/m^2]', 'FontSize', 14)
+% set(ax2,'Xscale','log','Zscale','log','Yscale','log')
+xlabel('Radiative Forcing [mW/m^2]', 'FontSize', 14)
 ylabel('Ozone Depletion [%]', 'FontSize', 14)
 zlabel('Cost [$]', 'FontSize', 14)
 title('Plot of Pareto Points (blue) with Dominated Solutions (red)', 'FontSize', 16)
@@ -117,7 +124,7 @@ plot(objective_vals(:,1), objective_vals(:,3), '.-', 'MarkerSize', 20)
 hold on
 scatter(filtered_populations(:,10), filtered_populations(:,12), 100, 'r.');
 hold off
-xlabel('Radiative Forcing [W/m^2]', 'FontSize', 14)
+xlabel('Radiative Forcing [mW/m^2]', 'FontSize', 14)
 ylabel('Cost [$]', 'FontSize', 14)
 legend({'Pareto Points/Front', 'Dominated Solutions'}, 'FontSize', 14)
 
@@ -141,7 +148,7 @@ plot(objective_vals(:,1), objective_vals(:,2), '.-', 'MarkerSize', 20)
 hold on
 scatter(filtered_populations(:,10), filtered_populations(:,11), 100, 'r.');
 hold off
-xlabel('Radiative Forcing [W/m^2]', 'FontSize', 14)
+xlabel('Radiative Forcing [mW/m^2]', 'FontSize', 14)
 ylabel('Ozone Depletion [%]', 'FontSize', 14)
 legend({'Pareto Points/Front', 'Dominated Solutions'}, 'FontSize', 14)
 
@@ -161,7 +168,7 @@ ylabel('Dominance Test Total Score', 'FontSize', 14)
 % reentry_shield_material_db = readtable("reentry_shield_materials.csv");
 % xopt = doe_res(trial_num).x_opt;
 reusability_combo = zeros(1, width(xopt));
-for p = 1:length(xopt)
+for p = 1:height(xopt)
     x= xopt(p, :);
     stg1_reusable = x(2);
     stg2_reusable = x(3);
@@ -175,7 +182,7 @@ for p = 1:length(xopt)
         reusability_combo(p) = 4;
     end
 end
-figure();
+figure(4);
 histogram(reusability_combo, [0.5, 1.5, 2.5, 3.5, 4.5, 5]);
 xlabel("Stage Reusability Configuration");
 ylabel("Count");
@@ -184,7 +191,7 @@ title("Reusability of Rocket Among Pareto Front Solutions");
 %% Stage Radius 
 %Stage Radius 
 radius_values = xopt(:, 7);
-figure();
+figure(5);
 histogram(radius_values, 3);
 xlabel("Rocket Radius (m)");
 ylabel("Count");
@@ -209,7 +216,7 @@ for id_index = 1:length(unique_ids)
     rebased_value = rebased_value +1;
 end
 
-figure();
+figure(6);
 histogram(rebased_data_stg1,[0.5, 1.5, 2.5] ); %hold on;
 %histogram(stg2_ep_values); 
 hold off;
@@ -225,7 +232,7 @@ for p = 1:length(pareto_solution_outputs)
     rocket = pareto_solution_outputs(p).rocket;
     rocket_heights(p) = rocket.stage1.height + rocket.stage2.height;
 end
-figure();
+figure(7);
 histogram(rocket_heights, 10);
 xlabel("Rocket Height (m)");
 ylabel("Count");
@@ -236,13 +243,13 @@ title("Rocket Heights in Pareto Front Solutions");
 stg1_propmass = xopt(:, 8);
 stg2_propmass = xopt(:, 9);
 total_propmass = stg1_propmass + stg2_propmass;
-pareto_front_ids = 1:length(xopt);
-figure();
+pareto_front_ids = 1:height(xopt);
+figure(8);
 scatter(pareto_front_ids, total_propmass, 16)
 
 %% Launch Cadence Plots
 colors = ['r', 'b', 'k', 'g', 'c', 'm', 'y', "#7E2F8E", "#a3957e", "#9F2B68"];
-figure()
+figure(9)
 for j = 1:length(pareto_solution_outputs)
     launch_cadence = pareto_solution_outputs(j).launch_cadence;
     cumulative_launch_cadence = cumsum(launch_cadence(1,:));
@@ -258,6 +265,33 @@ ax=gca;
 ax.FontSize = 20;
 grid on
 hold off
+
+function [objective_vals, pareto_point_idxs] = paretoFiltering(input_pts)
+    is_dominated = zeros(height(input_pts), 1);
+
+    for pt_num = 1:height(input_pts)
+        if is_dominated(pt_num) == 0
+            reference_obj = input_pts(pt_num, 10:12);
+
+            for comparison_pt = 1:height(input_pts)
+                if (is_dominated(comparison_pt) == 0) && (pt_num ~= comparison_pt)
+                    comp_res = reference_obj < input_pts(comparison_pt, 10:12);
+                    if sum(comp_res) == 3
+                        is_dominated(comparison_pt) = 1;
+                    end
+                end
+            end
+        end
+    end
+
+    pareto_point_idxs = find(is_dominated == 0);
+
+    input_pts(:, end+1) = is_dominated;
+    objective_vals = input_pts(input_pts(:,end)==0, 10:12);
+
+    
+end
+
 
 
 
