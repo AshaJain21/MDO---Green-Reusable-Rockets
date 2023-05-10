770c530910f820699e872e9e43abf4d79f7924f4
Justin <justinpws@gmail.com>
2023-05-09 22:10:07 -0400

    More minor cleanup to run_model_ga

--- run_model_ga.m
+++ run_model_ga.m
@@ -7,10 +7,9 @@ function penalized_cost = run_model_ga(x)
 
     design_variables = setup_designvariables(round(x(1)), round(x(2)), round(x(3)), engine_prop_db(engine_prop_1_row, :), engine_prop_db(engine_prop_2_row, :), reentry_shield_material_db(reentry_shield_material_row, :), x(7), x(8), x(9));
     parameters = setup_parameters();
-    g = 0;
-    h = 0;
+
     [~, ~, ~, ~, cost, ~, rocket] = run_model(design_variables, parameters);
 
-    penalized_cost  = sum(cost(1, :)) + g + h;
+    penalized_cost  = sum(cost(1, :));
 
 end
