c64bf765505abc5ceaee0772f1f09f89fc197f0d
Justin <justinpws@gmail.com>
2023-05-10 09:37:10 -0400

    Created a new script to run epsilon constrained version of single-objective GA

---
+++ Constraint Module/calculate_nonpenalty_constraints_rf_constr.m
@@ -0,0 +1,59 @@
+function [c, ceq] = calculate_nonpenalty_constraints_rf_constr(x, rf_constraint)
+
+%     global rf_constraint
+  
+    engine_prop_1_row = round(x(4));
+    engine_prop_2_row = round(x(5));
+    engine_prop_db = readtable("engine-prop-combinations.csv");
+    reentry_shield_material_row = round(x(6));
+    reentry_shield_material_db = readtable("reentry_shield_materials.csv");
+
+    design_variables = setup_designvariables(round(x(1)), round(x(2)), round(x(3)), engine_prop_db(engine_prop_1_row, :), engine_prop_db(engine_prop_2_row, :), reentry_shield_material_db(reentry_shield_material_row, :), x(7), x(8), x(9));
+    parameters = setup_parameters();
+
+    [launch_cadence, total_rf, ~, ~, cost, ~, rocket] = run_model(design_variables, parameters);
+   
+    c =[];
+    ceq = [];
+
+   %Constraint on Launch Cadence 
+   total_time = sum(launch_cadence(1,:));
+   c(end+1) =  total_time - (parameters.delivery_time * 12); %months 
+
+
+  %Constraint on Cost
+  num_years = ceil(total_time/12);
+  budget_per_year = zeros(1, num_years);
+  
+  for curr_year = 1:num_years
+      curr_year_costs = cost(:, (cost(2,:)<=(curr_year*12) & cost(2,:)>((curr_year-1)*12)) );
+      budget_per_year(curr_year) = sum(curr_year_costs(1,:));
+  end
+
+  max_cost_per_year = max(budget_per_year);
+  c(end+1) = max_cost_per_year - parameters.max_cost_per_year;
+
+
+   %Constraint on rocket height 
+   total_rocket_height = rocket.stage1.height + rocket.stage2.height;
+   c(end+1) = total_rocket_height - parameters.max_rocket_height; 
+
+   %Constraint on payload height in relation to rocket height
+   c(end+1) = rocket.payload_height - (parameters.max_payload_height_fraction*total_rocket_height);
+
+   %Constraint on mprop1 and mprop1_guess
+   c(end+1) = rocket.stage1.mprop - design_variables.mprop1_guess;
+   c(end+1) = rocket.stage2.mprop - design_variables.mprop2_guess;
+
+   %Constraint num_engines
+   c(end+1) = parameters.min_num_engines - rocket.stage1.nEng;
+   c(end+1) = rocket.stage1.nEng - parameters.max_num_engines;
+
+   %Constraint on engine size relative to rocket radius
+   c(end+1) = design_variables.stage1.engine_prop{1,8} - design_variables.rocket_ri;
+   c(end+1) = design_variables.stage2.engine_prop{1,8} - design_variables.rocket_ri;
+
+   %Calculate the constraint on rf
+   c(end+1) = total_rf - rf_constraint;
+
+end
---
+++ run_ga_rf_constr.m
@@ -0,0 +1,52 @@
+clc;clear;
+addpath(genpath(pwd))
+problem.fitnessfcn = @run_model_ga;
+problem.nvars = 9;
+%           [# launch,reuse1,reuse2,engine1,engine2,re-entry mat, ri ,  mprop1, mprop2]
+problem.lb =[12,       0,      0,      1,      1,         1,      0.8,  7000,    1000]; 
+problem.ub =[1500,     1,      1,      16,     16,        11,     4.5,  4e6,     1.5e6];
+problem.solver = 'ga';
+
+problem.intcon = [1,2,3,4,5,6];
+rf_constraint_opts = logspace(log10(0.1), log10(100), 20);
+rf_constraint_opts = flip(rf_constraint_opts)
+pop_size_opts = [500];
+num_trials = 1;
+total_trials = length(pop_size_opts) * num_trials * length(rf_constraint_opts);
+
+%Initialize arrays for results
+doe_res = zeros(total_trials, (3+problem.nvars+3));
+exp_num = 1;
+output_full = struct(problemtype='', rngstate=struct(), generations=0, funccount=0, message='', maxconstraint=0, hybridflag=[]);
+population_data_full = zeros(1, (problem.nvars+1));
+
+% global rf_constraint
+
+for i = 1:length(pop_size_opts)
+    for j = 1:num_trials
+        for k = 1:length(rf_constraint_opts)
+            pop_size = pop_size_opts(i);
+            rf_constraint = rf_constraint_opts(k);
+            problem.nonlcon = @(x)calculate_nonpenalty_constraints_rf_constr(x, rf_constraint);
+    
+            options = optimoptions('ga', 'PopulationSize', pop_size, 'UseParallel', true, 'UseVectorized', false);%, 'PlotFcn',{@gaplotstopping, @gaplotscores});%  'MutationFcn', mutation_settings, 'ConstraintTolerance', 1e-1);
+            problem.options = options;
+    
+            fprintf('======= Current Trial: Population size: %d, Trial: %d ============\n', pop_size, j);
+    
+            tstart = tic;
+            [x_opt,fval,exitflag,output,population,scores]  = ga(problem);
+            comp_time = toc(tstart);
+    
+            % Store Values
+            doe_res(exp_num, :) = [pop_size, j, rf_constraint, x_opt, exitflag, fval, comp_time];
+            marker_row = ones(1, (problem.nvars+1))*exp_num;
+            population_data_full = [population_data_full; marker_row;[population, scores]];
+            output_full = [output_full, output];
+            exp_num = exp_num + 1;
+        end
+    end
+end
+
+%Filter any rows with exit val of -2
+filtered_doe_res = doe_res(doe_res(:,13)>=0,:);
