45ca39712b3f7ca17ad6260bde415499e6781e34
Justin <justinpws@gmail.com>
2023-05-09 22:09:26 -0400

    Minor cleanup to unused code in run_model_ga

--- run_model_ga.m
+++ run_model_ga.m
@@ -11,34 +11,6 @@ function penalized_cost = run_model_ga(x)
     h = 0;
     [~, ~, ~, ~, cost, ~, rocket] = run_model(design_variables, parameters);
 
-%     if isfile('rocket_results_ga.mat')
-%         load('rocket_results_ga');
-%         rockets_all = [rockets_all, rocket];
-%     else
-%         rockets_all = rocket;
-%     end
-% 
-%     save('rocket_results_ga', 'rockets_all');
-%     [g, h] = calculate_penalties(constraints);
     penalized_cost  = sum(cost(1, :)) + g + h;
-
-end
-
-function [g, h] = calculate_penalties(constraints)
-   g = 0;
-   h = 0;
-    %Inequality Constraints 
-    g = g + max(0, constraints.launch_cadence);
-%     g = g + max(0, constraints.max_cost_year);
-    g = g + max(0, constraints.rocket_height);
-    g = g + max(0, constraints.payload_height);
-    g = g + max(0, constraints.min_stg1_num_engines);
-    g = g + max(0, constraints.max_stg1_num_engines);
-    g = g + (max(0, constraints.eng_stg1_rocket_size)*100);
-    g = g + (max(0, constraints.eng_stg2_rocket_size)*100);
-
-    %Equality Constraints 
-    h = h + (constraints.mprop1)^2;
-    h = h + (constraints.mprop2) ^2;
 
 end
